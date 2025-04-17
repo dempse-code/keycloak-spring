70789ebbc5446e232a18d08768102ec12a863b84
dempse-code <ulfufey@mail.ru>
2025-04-17 10:00:22 +0700

    init commit

---
+++ src/main/java/ru/lebedev/faktura/FakturaApplication.java
@@ -0,0 +1,13 @@
+package ru.lebedev.faktura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakturaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FakturaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/lebedev/faktura/FakturaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lebedev.faktura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakturaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
